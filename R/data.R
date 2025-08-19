b5b9c89222ac13aa800533f4892b6336943034a8
tk <tkimhofer@gmail.com>
2025-08-19 19:39:46 +0200

    char set upd docstring, minor upd description

--- R/data.R
+++ R/data.R
@@ -1,12 +1,25 @@
 #' @name bariatric
 #' @docType data
 #' @title Bariatric surgery data
-#' @author Torben Kimhofer \email{tkimhofer@@murdoch.edu.au}
 #' @usage data(bariatric)
 #' @description Metabolic profiling NMR dataset of murine urine samples collected before and after bariatric surgery (Roux-en-Y gastric bypass).
-#' @details One dimensional proton NMR spectra of murine urine samples, acquired on a Bruker Avance III spectrometer (Bruker, Rheinstetten, Germany) operating at a frequency of 600.13 MHz and a temperature of 300 K. A standard NMR pulse sequence (\emph{recycle delay-90°-t1-90°-tm-90°-acquisition}) was applied to acquire one-dimensional Proton NMR spectral data, where \emph{t1} was set to 3 μs and \emph{tm} (mixing time) was set to 100 ms. Water suppression was achieved using selective irradiation during a \emph{recycle delay} of 2 s and \emph{tm}. A 90° pulse was adjusted to 10 μs. A total of 128 scans were collected into 64 k data points with a spectral width of 20 ppm.
+#' @details
+#' One-dimensional proton NMR spectra of murine urine samples were acquired on a
+#' Bruker Avance III spectrometer (Bruker, Germany) operating at
+#' 600.13 MHz and 300 K. A standard one-dimensional NOESY-presaturation pulse
+#' sequence (\emph{recycle delay–90\eqn{^\circ}{deg}–t1–90\eqn{^\circ}{deg}–tm–90\eqn{^\circ}{deg}–acquisition})
+#' was applied for water suppression and metabolite detection.
 #'
-#' For further information on sample collection and processing please see reference below. Pre-processing of raw NMR data is described in the \emph{MetaboMate} vignette Data Import and Preprocessing.
+#' Experimental parameters were as follows: \emph{t1} (short delay) was set to
+#' 3 \eqn{\mu}{micro}s, the mixing time (\emph{tm}) was 100 ms, and the recycle
+#' delay was 2 s. The 90\eqn{^\circ}{deg} pulse was calibrated to 10 \eqn{\mu}{micro}s.
+#' A total of 128 transients (scans) were collected into 64k complex data points,
+#' with a spectral width of 20 ppm.
+#'
+#' Water suppression was achieved by selective irradiation during both the
+#' recycle delay and the mixing time. For further information on sample
+#' collection and preprocessing, see the reference below and the
+#' \emph{metabom8} vignette “Data Import and Preprocessing”.
 #' @format A list with three elements:
 #' \describe{
 #'   \item{X.pqn}{Matrix of pre-processed Proton NMR spectra with rows representing spectra}
@@ -14,7 +27,9 @@
 #'   \item{meta}{Spectrometer metadata}
 #' }
 #' @source Dr. Jia V. Li, Imperial College London, Department of Surgery and Cancer, Centre for Computational and Systems Medicine. \url{https://www.imperial.ac.uk/people/jia.li}
-#' @references Li, Jia V., \emph{et al.} (2011) Metabolic surgery profoundly influences gut microbial-host metabolic cross-talk. \emph{Gut}. 60.9, 1214-1223.
-#' @references \url{https://www.ncbi.nlm.nih.gov/pubmed/21572120}
+#' @references Li, Jia V., \emph{et al.} (2011)
+#' Metabolic surgery profoundly influences gut microbial-host metabolic cross-talk.
+#' \emph{Gut}, 60(9), 1214–1223. \doi{10.1136/gut.2010.234708}
+#' PMID: \url{https://pubmed.ncbi.nlm.nih.gov/21572120}
 #' @keywords datasets
 "bariatric"
